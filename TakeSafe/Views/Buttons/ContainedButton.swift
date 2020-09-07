7f2198cec7ea856cd9c5e08706258344b79d0ba2
Linus Långberg <git@phoqe.dev>
2020-09-07 18:47:35 +0200

    Add ContainedButton

---
+++ TakeSafe/Views/Buttons/ContainedButton.swift
@@ -0,0 +1,33 @@
+//
+//  ContainedButton.swift
+//  TakeSafe
+//
+//  Created by Linus Långberg on 2020-09-07.
+//
+
+import SwiftUI
+
+struct ContainedButton: View {
+    var title: String
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action, label: {
+            Text(title)
+                .font(.body)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .padding()
+        })
+        .background(Color.blue)
+        .cornerRadius(5)
+    }
+}
+
+struct ContainedButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ContainedButton(title: "Get Started", action: {
+            
+        })
+    }
+}
