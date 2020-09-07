80b1635ad07457f0b1abee20edb52b92b2c2ca76
Linus Långberg <git@phoqe.dev>
2020-09-07 10:40:39 +0200

    Initial Commit

---
+++ TakeSafe/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TakeSafe
+//
+//  Created by Linus Långberg on 2020-09-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TakeSafe/TakeSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeSafeApp.swift
+//  TakeSafe
+//
+//  Created by Linus Långberg on 2020-09-07.
+//
+
+import SwiftUI
+
+@main
+struct TakeSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
