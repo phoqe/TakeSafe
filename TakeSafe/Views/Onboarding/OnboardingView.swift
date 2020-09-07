55383320b20d3fd20cfa0eeb2ea2f354547c79a2
Linus Långberg <git@phoqe.dev>
2020-09-07 15:19:24 +0200

    Add translations for Swedish

--- TakeSafe/Views/Onboarding/OnboardingView.swift
+++ TakeSafe/Views/Onboarding/OnboardingView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct OnboardingView: View {
     var body: some View {
         PageView([
-            OnboardingScreen(image: "Medical Care", title: "TakeSafe. Take drugs safely.", description: "Trouble sleeping? Feeling fatigued? Take control of your life by tracking the motion and effects of drugs like caffeine. All from your iPhone."),
+            OnboardingScreen(image: "Medical Care", title: NSLocalizedString("onboardingScreen1Title", comment: ""), description: NSLocalizedString("onboardingScreen1Description", comment: "")),
             
-            OnboardingScreen(image: "Time Management", title: "From absorption to excretion.", description: "Follow the time course of a drug’s absorption, bioavailability, distribution, metabolism, and excretion. Educated guesses based on numerous factors."),
+            OnboardingScreen(image: "Time Management", title: NSLocalizedString("onboardingScreen2Title", comment: ""), description: NSLocalizedString("onboardingScreen2Description", comment: "")),
             
-            OnboardingScreen(image: "Doctors", title: "Realtime advice from experts.", description: "Another cup of coffee? We’ll tell you how it will impact the rest of your day and health and if it’ll disturb your sleep. Because sleep is important.")
+            OnboardingScreen(image: "Doctors", title: NSLocalizedString("onboardingScreen3Title", comment: ""), description: NSLocalizedString("onboardingScreen3Description", comment: ""))
         ])
     }
 }
