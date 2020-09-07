fd507cb9bdcda0fa062453f13569f730d43c94fe
Linus Långberg <git@phoqe.dev>
2020-09-08 00:08:47 +0200

    Add conditional

--- TakeSafe/Views/Onboarding/OnboardingView.swift
+++ TakeSafe/Views/Onboarding/OnboardingView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @State var state: OnboardingState = .intro
+    
+    let introPages = [
+        OnboardingPage(image: "Medical Care", title: NSLocalizedString("onboardingScreen1Title", comment: ""), description: NSLocalizedString("onboardingScreen1Description", comment: "")),
+        
+        OnboardingPage(image: "Time Management", title: NSLocalizedString("onboardingScreen2Title", comment: ""), description: NSLocalizedString("onboardingScreen2Description", comment: "")),
+        
+        OnboardingPage(image: "Doctors", title: NSLocalizedString("onboardingScreen3Title", comment: ""), description: NSLocalizedString("onboardingScreen3Description", comment: "")),
+        
+        OnboardingPage(image: "Profile Details", title: NSLocalizedString("onboardingScreen4Title", comment: ""), description: NSLocalizedString("onboardingScreen4Description", comment: ""))
+    ]
+    
+    let setupPages = [
+        OnboardingPage(image: "Profile Details", title: NSLocalizedString("onboardingScreen4Title", comment: ""), description: NSLocalizedString("onboardingScreen4Description", comment: ""))
+    ]
+    
     var body: some View {
-        OnboardingPageView([
-            OnboardingPage(image: "Medical Care", title: NSLocalizedString("onboardingScreen1Title", comment: ""), description: NSLocalizedString("onboardingScreen1Description", comment: "")),
-            
-            OnboardingPage(image: "Time Management", title: NSLocalizedString("onboardingScreen2Title", comment: ""), description: NSLocalizedString("onboardingScreen2Description", comment: "")),
-            
-            OnboardingPage(image: "Doctors", title: NSLocalizedString("onboardingScreen3Title", comment: ""), description: NSLocalizedString("onboardingScreen3Description", comment: "")),
-            
-            OnboardingPage(image: "Profile Details", title: NSLocalizedString("onboardingScreen4Title", comment: ""), description: NSLocalizedString("onboardingScreen4Description", comment: ""))
-        ])
+        if state == .intro {
+            OnboardingPageView(introPages)
+        } else if state == .setup {
+            OnboardingPageView(setupPages)
+        }
     }
 }
 
