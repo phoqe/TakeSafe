c195ea15f88e8dbe9cf7df2f6075f435eb8d3775
Linus Långberg <git@phoqe.dev>
2020-10-03 17:26:03 +0200

    Remove Firebase

--- TakeSafe/AppDelegate.swift
+++ TakeSafe/AppDelegate.swift
@@ -6,12 +6,9 @@
 //
 
 import UIKit
-import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        
         return true
     }
 }
