92f63f4cda815d03a8e9a792449502bdb7781783
Linus Långberg <git@phoqe.dev>
2020-10-12 19:04:35 +0200

    Add navigation bar title

--- TakeSafe/Views/Settings/AppleHealthView.swift
+++ TakeSafe/Views/Settings/AppleHealthView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct AppleHealthView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Apple Health")
+            .navigationBarTitle("Apple Health")
     }
 }
 
