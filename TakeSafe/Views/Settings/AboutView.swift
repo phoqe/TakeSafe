bbed4f2079c1f2257e2de3913b65a9d6e678896f
Linus Långberg <git@phoqe.dev>
2020-10-12 16:04:06 +0200

    Fix crash

--- TakeSafe/Views/Settings/AboutView.swift
+++ TakeSafe/Views/Settings/AboutView.swift
@@ -10,14 +10,13 @@ import SwiftUI
 struct AboutView: View {
     let name = Bundle.main.infoDictionary!["CFBundleName"] as! String
     let version = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
-    let website = ProcessInfo.processInfo.environment["website"]!
     
     var body: some View {
         Form {
             Section(header: Text("Links")) {
-                Link("Website", destination: URL(string: website)!)
-                Link("Terms and Conditions", destination: URL(string: "\(website)/terms")!)
-                Link("Privacy Policy", destination: URL(string: "\(website)/privacy")!)
+                Link("Website", destination: URL(string: "https://takesafe.app")!)
+                Link("Terms and Conditions", destination: URL(string: "https://takesafe.app/terms")!)
+                Link("Privacy Policy", destination: URL(string: "https://takesafe.app/privacy")!)
             }
             
             Section(header: Text("Version")) {
