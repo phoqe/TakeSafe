fb63a6c18683acce0dea51c2fec053c1648dae50
Linus Långberg <git@phoqe.dev>
2020-10-12 13:11:14 +0200

    Improve

--- TakeSafe/Views/Settings/SettingsView.swift
+++ TakeSafe/Views/Settings/SettingsView.swift
@@ -11,8 +11,8 @@ import HealthKit
 struct SettingsView: View {
     @State var height = UserDefaults.standard.string(forKey: "height") ?? ""
     @State var weight = UserDefaults.standard.string(forKey: "weight") ?? ""
-    @State var bedtime = Date()
-    @State var wakeUpTime = Date()
+    @State var goToSleepTime = UserDefaults.standard.data(forKey: "goToSleepTime") as! Date? ?? Date()
+    @State var wakeUpTime = UserDefaults.standard.data(forKey: "wakeUpTime") as! Date? ?? Date()
     
     let isAppleHealthConnected = UserDefaults.standard.bool(forKey: "isAppleHealthConnected")
     let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
@@ -49,7 +49,7 @@ struct SettingsView: View {
                 }
                 
                 Section(header: Text("Sleep"), footer: Text("We use your sleep and wake up time to present information when drugs may disturb your sleep.")) {
-                    DatePicker("Go to sleep", selection: $bedtime, displayedComponents: .hourAndMinute)
+                    DatePicker("Go to sleep", selection: $goToSleepTime, displayedComponents: .hourAndMinute)
                     DatePicker("Wake up", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
                 }
                 
