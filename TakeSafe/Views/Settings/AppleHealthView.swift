09d9471cc742a0bb1ca98c90e7cbb2339937b11f
Linus Långberg <git@phoqe.dev>
2020-10-27 16:06:08 +0100

    Fix Apple Health view more

--- TakeSafe/Views/Settings/AppleHealthView.swift
+++ TakeSafe/Views/Settings/AppleHealthView.swift
@@ -12,30 +12,30 @@ struct AppleHealthView: View {
         VStack {
             Image("Doctors")
                 .resizable()
-                .frame(width: 300, height: 225, alignment: .center)
-                .padding()
+                .frame(width: 300, height: 225)
+                .padding(.vertical)
             
             VStack {
                 Text("Connect us with Apple Health.")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .foregroundColor(.primary)
+                    .font(.title3)
+                    .fontWeight(.semibold)
                     .padding(.bottom, 2.5)
                 
                 Text("We can use data from Apple Health to give you info and advice in realtime. For example, we’ll tell you if a drug will disturb your sleep before you take it.")
-                    .font(.body)
                     .foregroundColor(.secondary)
             }
             .padding(.horizontal, 30)
-            .padding(.bottom, 20)
+            .padding(.bottom, 40)
             
             Button(action: {
                 
             }, label: {
                 Text("Connect Apple Health")
-                    .font(.title3)
+                    .font(.headline)
                     .fontWeight(.semibold)
             })
+            
+            Spacer()
         }
         .navigationBarTitle("Apple Health")
     }
