fe1d8effa5f988ea618313512ee8e6312d03dd25
Linus Långberg <git@phoqe.dev>
2020-11-22 17:18:49 +0100

    Group in drug view

--- TakeSafe/Views/Drugs/DrugList.swift
+++ TakeSafe/Views/Drugs/DrugList.swift
@@ -8,12 +8,33 @@
 import SwiftUI
 
 struct DrugList: View {
-    var drugs: [Drug]
-    
+    var stimulants: [Drug]
+    var hallucinogens: [Drug]
+
+    init(drugs: [Drug]) {
+        self.stimulants = drugs.filter { $0.drugClass == .stimulant }
+        self.hallucinogens = drugs.filter { $0.drugClass == .hallucinogen }
+    }
+
     var body: some View {
-        List(drugs) { drug in
-            DrugListItem(drug: drug)
+        List {
+            if !stimulants.isEmpty {
+                Section(header: Text("Stimulants").textCase(.none)) {
+                    ForEach(stimulants) { stimulant in
+                        DrugListItem(drug: stimulant)
+                    }
+                }
+            }
+
+            if !hallucinogens.isEmpty {
+                Section(header: Text("Hallucinogens").textCase(.none)) {
+                    ForEach(hallucinogens) { hallucinogen in
+                        DrugListItem(drug: hallucinogen)
+                    }
+                }
+            }
         }
+        .padding(.top)
         .listStyle(InsetGroupedListStyle())
     }
 }
